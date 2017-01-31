225fae36769fceec96ab9076bf96a7a6a7433403
vorvit1980 <vorvit1980@gmail.com>
2017-02-01 00:54:12 +0300

    Update plugins

--- app/src/main/java/com/example/vorvit1980/reminder/MainActivity.java
+++ app/src/main/java/com/example/vorvit1980/reminder/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);// только portrait
 
         PreferenceHelper.getInstance().init(getApplicationContext());
         preferenceHelper = PreferenceHelper.getInstance();
